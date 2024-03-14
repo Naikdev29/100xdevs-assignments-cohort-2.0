97fdeda7f04c07b7e5d333646adabaa22731c02a
Naikdev29 <devnaik75@gmail.com>
2024-03-14 11:45:25 -0400

    solution Calculate time

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,13 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const timeStamp = Date.now();
+    let sum =0;
+    for (let i=0; i<=n; i++)[
+            sum++
+    ]
+const stamp = Date.now() - timeStamp;
+    console.log(`Time is ${stamp / 1000}`);
+    return true;
 }
+module.exports = calculateTime;
